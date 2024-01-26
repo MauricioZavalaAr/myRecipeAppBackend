a94d0fda7e652bcf61351744303d95217767a5ba
Mauricio Zavala <mauricio.zavala.ar@gmail.com>
2024-01-25 22:00:48 -0600

    change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Make sure you have installed the CORS package
+const cors = require('cors');
 
 const recipeRoutes = require('./routes/recipes');
 const userRoutes = require('./routes/user');
@@ -9,28 +9,26 @@ require('dotenv').config();
 const app = express();
 
 // Enable CORS for all origins
-app.use(cors({
-  origin: '*', // Allows all origins
-  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'], // Allows these methods
-  credentials: true // Allows credentials (cookies, authorization headers, etc.)
-}));
+app.use(cors());
 
 app.use(express.json()); // for parsing application/json
 
+// Routes
 app.use('/user', userRoutes);
 app.use('/recipes', recipeRoutes);
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Successfully connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('An error occurred!');
+  res.status(500).send('Something broke!');
 });
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
